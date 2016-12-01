cf457119bc6e15f29762f505f37d7a0e5a5563b4
nibin <nibin.k.paul@gmail.com>
2016-12-01 10:08:40 +0530

    Required field | index error fixed

--- cookie/cokkie_site/forms.py
+++ cookie/cokkie_site/forms.py
@@ -4,12 +4,12 @@ class InputForm(forms.Form):
     """
     form for Entering the cookie to be processed
     """
-    input = forms.CharField(max_length=1000, required=True,
+    input = forms.CharField(max_length=1000, required=False,
                                  widget=forms.Textarea(attrs={'placeholder': 'Input Here',
                                                                'name': 'input',
                                                                'id': 'input',
                                                                }))
-    output = forms.CharField(max_length=1000,
+    output = forms.CharField(max_length=1000,required=False,
                                  widget=forms.Textarea(attrs={'placeholder': 'Formatted cookie here',
                                                                'name': 'output',
                                                                'id': 'output',
